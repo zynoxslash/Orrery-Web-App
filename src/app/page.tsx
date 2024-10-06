9e57a5d346f6c6c8a8ded65235ebf6d04def5350
ZynoxSlash <mehmetemintarlaci@gmail.com>
2024-10-06 16:06:01 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,24 +23,6 @@ export default function Page() {
             </div>
           </div>
         </div>
-        <div className='flex absolute mt-[10rem] mb-32 rounded-2xl w-[90%] h-[13rem] bg-indigo-500 overflow-visible'>
-          {[1, 2, 3, 4].map((m, a) => (
-            <div key={a} className='hover:cursor-pointer relative group/card flex flex-col'>
-              <div
-                className='transition-all group-hover/card:scale-110 group-hover/card:rounded-xl w-[16rem] ml-12 
-                        -mt-24 h-[9rem] bg-indigo-700 z-30 relative grid place-content-center text-white'
-              > 
-                <TbPlayerPlayFilled className="w-10 h-10"/>
-              </div>
-              <div className='mt-3 ml-12'>
-                <h1 className='text-2xl font-semibold'>Information</h1>
-                <p className='max-w-[16rem] italic text-indigo-900'>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat nam maiores recusandae
-                </p>
-              </div>
-            </div>
-          ))}
-        </div>
       </div>
     </main>
   )
